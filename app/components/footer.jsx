f96295cd65ff01072085a13bfa7b982203294bbf
ahmed <ah7532494@gmail.com>
2024-09-14 21:36:25 +0500

    minor changes

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/saad-raj4/" className="text-[#16f2b3]">Saad Raja</Link>
+            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/ahmed-hassan-82964824a" className="text-[#16f2b3]">Ahmed Hassan</Link>
           </p>
           <div className="flex items-center gap-5">
               <Link
